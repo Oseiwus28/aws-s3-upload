fa773a58f005f2ba1fa228161622362f38bbefbd
oseiwus28 <oseiwusu28@gmail>
2023-09-25 08:13:51 +0000

    Added a note

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3")
-const fs = require("fs")
-const dotEnv = require("dotenv").config()
-
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const dotEnv = require("dotenv").config();
 
 const s3Client = new S3Client({
-    region:process.env.REGION,
-    endpoint:process.env.ENDPOINT,
-    credentials:{
-        accessKeyId:process.env.ACCESSKEYID,
-        secretAccessKey:process.env.SECRETACCESSKEY
-    }
-})
+  region: process.env.REGION,
+  endpoint: process.env.ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.ACCESSKEYID,
+    secretAccessKey: process.env.SECRETACCESSKEY,
+  },
+});
 
-const uploadFile = async()=>{
-    try{
-        const fileContent = fs.readFileSync("pic.jpg")
-        const object = new PutObjectCommand({
-            Bucket:"aws-s3-fileupload",
-            Key:"pic.jpg",
-            Body: fileContent
-        });
-        await s3Client.send(object)
-    }catch(e){
-        console.log(e)
-    }
-}
+const uploadFile = async () => {
+  try {
+    const fileContent = fs.readFileSync("pic.jpg");
+    const object = new PutObjectCommand({
+      Bucket: "aws-s3-fileupload",
+      Key: "pic.jpg",
+      Body: fileContent,
+    });
+    await s3Client.send(object);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-uploadFile()
+uploadFile();
